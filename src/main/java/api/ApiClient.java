5ff1d2fef88e169e25afec18115a017154fdfd99
Rafael Oliveira <bellotti.rafael@gmail.com>
2020-04-11 11:04:43 -0700

    added google http and oauth libs

--- src/main/java/api/ApiClient.java
+++ src/main/java/api/ApiClient.java
@@ -1,4 +1,28 @@
 package api;
 
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+
+import java.io.IOException;
+import java.util.Map;
+
 public class ApiClient {
+
+    public static HttpResponse getRequest(String endpoint, Map<String, ?> params) throws IOException {
+        GenericUrl url = new GenericUrl(endpoint);
+        url.putAll(params);
+
+        HttpTransport transport = new NetHttpTransport();
+        Credential credential =
+                new Credential(BearerToken.authorizationHeaderAccessMethod()).setAccessToken("accessToken");
+        HttpRequestFactory requestFactory = transport.createRequestFactory(credential);
+
+        return requestFactory.buildGetRequest(url).execute();
+
+    }
 }
