fc9330ebc4f04842035d4aba02463ea08b6e1efb
Nicolas Grantham <nicalgrant@gmail.com>
2020-04-19 09:04:16 -0700

    added base component

--- src/main/java/components/BaseComponent.java
+++ src/main/java/components/BaseComponent.java
@@ -1,4 +1,53 @@
 package components;
 
-public class BaseComponent {
+import api.ApiClient;
+import com.google.gson.Gson;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+public class BaseComponent extends ApiClient {
+
+
+    public BaseComponent(String baseUri, Integer timeout) throws UnirestException {
+        super(baseUri, timeout);
+    }
+
+    /**
+     * HTTP POST method request
+     *
+     * @param endpoint Path URL
+     * @param params Query parameters
+     * @param data Request body
+     * @return HttpResponse
+     * @throws URISyntaxException
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public HttpResponse postRequest(String endpoint, List<NameValuePair> params, Map<String, Object> data) throws URISyntaxException, IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        Gson gson = new Gson();
+
+        URIBuilder uriBuilder = new URIBuilder(urlFor(endpoint));
+        if (params != null) {
+            uriBuilder.addParameters(params);
+        }
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uriBuilder.toString()))
+                .setHeader("Authorization", "Bearer " + getToken())
+                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(data)))
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
 }
