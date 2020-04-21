906deac90f1e67117f084ddd084dc19cc08c33b2
Nicolas Grantham <nicalgrant@gmail.com>
2020-04-21 00:04:39 -0700

    print chat channel in oauthbot

--- src/main/java/bots/OAuthBot.java
+++ src/main/java/bots/OAuthBot.java
@@ -12,6 +12,10 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 public class OAuthBot {
 
@@ -35,6 +39,48 @@ public class OAuthBot {
         System.out.println(user);
         System.out.println("----------");
 
+//        FIXME: SOMETHING NOT RIGHT WHEN TRYING TO SEND A MESSAGE
+
+        HttpResponse channelListResponse = client.getChatChannels().listChannels( null);
+        String[] channelList = gson.toJson(channelListResponse.body()).split("\\\\");
+        for(String channels : channelList) {
+            System.out.println(channels);
+        }
+
+        for(String channels : channelList) {
+            if(channels.contains("test")) {
+                String channelName = channels.substring(1,channels.length());
+                System.out.println(channelName);
+                boolean flag = true;
+                while(flag) {
+                    Scanner scan = new Scanner(System.in);
+                    System.out.println("Enter Something: ");
+                    String mess = scan.next();
+                    if(mess.equals("stop"))
+                        break;
+                    HashMap<String, Object> message = new HashMap<>();
+                    message.put("message",mess);
+                    System.out.println((client.getChatMessages().putMessage("109ab13498c64fd5911a42be1076ea6b", null, message)));
+                }
+            }
+        }
         tunnel.close();
     }
 }
+/*
+print(json.loads(client.meeting.list(user_id="me").content))
+client.chat_channels.list()
+channels = json.loads(client.chat_channels.list().content)["channels"]
+print(channels)
+for c in channels:
+    print(c)
+    if "test" in c.values():
+        print("Found channel test", c["id"])
+        cid = to_channel=c["id"]
+stop = False
+while not stop:
+    message = input("Enter message: ")
+    print(client.chat_messages.post(to_channel=cid, message=message))
+    if message == "stop":
+        stop = True
+ */
