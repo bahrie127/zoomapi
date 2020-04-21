fe3f253d023c5c724cc29e68e65dfc2611356345
Nicolas Grantham <nicalgrant@gmail.com>
2020-04-21 15:48:30 -0700

    report v1

--- src/main/java/components/Report.java
+++ src/main/java/components/Report.java
@@ -15,14 +15,12 @@ import java.util.Map;
 public class Report {
 
     public HttpResponse getAccountReport(List<String> userValues, List<NameValuePair> params, Map<String, Object> data) throws InterruptedException, IOException, URISyntaxException {
-        RequireKeys.requireKeys(userValues, Arrays.asList("start_time", "end_time"));
         data.put("from", DateToString.dateToString(userValues.get(0)));
         data.put("to",DateToString.dateToString(userValues.get(1)));
         return ApiClient.getInstance().postRequest("report/getaccountreport", params, data);
     }
 
     public HttpResponse getUserReport(List<String> userValues, List<NameValuePair> params, Map<String, Object> data) throws InterruptedException, IOException, URISyntaxException {
-        RequireKeys.requireKeys(userValues, Arrays.asList("start_time", "end_time"));
         data.put("from", DateToString.dateToString(userValues.get(0)));
         data.put("to",DateToString.dateToString(userValues.get(1)));
         return ApiClient.getInstance().postRequest("report/getuserreport", params, data);
