20a42c0d2bf6167c9627035124f72da752c333c2
Nicolas Grantham <nicalgrant@gmail.com>
2020-04-21 14:26:03 -0700

    fixed array index

--- src/main/java/components/Report.java
+++ src/main/java/components/Report.java
@@ -16,15 +16,15 @@ public class Report {
 
     public HttpResponse getAccountReport(List<String> userValues, List<NameValuePair> params, Map<String, Object> data) throws InterruptedException, IOException, URISyntaxException {
         RequireKeys.requireKeys(userValues, Arrays.asList("start_time", "end_time"));
-        data.put("from", DateToString.dateToString(userValues.get(1)));
-        data.put("to",DateToString.dateToString(userValues.get(2)));
+        data.put("from", DateToString.dateToString(userValues.get(0)));
+        data.put("to",DateToString.dateToString(userValues.get(1)));
         return ApiClient.getInstance().postRequest("report/getaccountreport", params, data);
     }
 
     public HttpResponse getUserReport(List<String> userValues, List<NameValuePair> params, Map<String, Object> data) throws InterruptedException, IOException, URISyntaxException {
         RequireKeys.requireKeys(userValues, Arrays.asList("start_time", "end_time"));
-        data.put("from", DateToString.dateToString(userValues.get(1)));
-        data.put("to",DateToString.dateToString(userValues.get(2)));
+        data.put("from", DateToString.dateToString(userValues.get(0)));
+        data.put("to",DateToString.dateToString(userValues.get(1)));
         return ApiClient.getInstance().postRequest("report/getuserreport", params, data);
     }
 }
