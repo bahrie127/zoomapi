3076ed83b194603f6a52293381d1e1714e13b180
Nicolas Grantham <nicalgrant@gmail.com>
2020-04-20 20:37:35 -0700

    created Util package, datetostring method, requirekeys method, updated meetingv2

--- src/main/java/components/MeetingV2.java
+++ src/main/java/components/MeetingV2.java
@@ -2,7 +2,8 @@ package components;
 
 import api.ApiClient;
 import org.apache.http.NameValuePair;
-
+import Util.DateToString;
+import Util.RequireKeys;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
@@ -11,23 +12,34 @@ import java.util.Map;
 
 public class MeetingV2 {
 
-    public HttpResponse list(String userID, List<NameValuePair> params) throws InterruptedException, IOException, URISyntaxException {
+    public HttpResponse list(List<String> userID, List<NameValuePair> params) throws InterruptedException, IOException, URISyntaxException {
+        RequireKeys.requireKeys(userID, "user_id");
         return ApiClient.getInstance().getRequest("/users/"+userID+"/meetings", params);
     }
 
-    public HttpResponse create(String userID, List<NameValuePair> params, Map<String, Object> data) throws InterruptedException, IOException, URISyntaxException {
+    public HttpResponse create(List<String> userID, List<NameValuePair> params, Map<String, Object> data) throws InterruptedException, IOException, URISyntaxException {
+        if(data.get("start_time") != null) {
+            data.put("start_time", DateToString.dateToString(data));
+        }
+        RequireKeys.requireKeys(userID, "user_id");
         return ApiClient.getInstance().postRequest("/users/"+userID+"/meetings", params, data);
     }
 
-    public HttpResponse get(String id, List<NameValuePair> params) throws InterruptedException, IOException, URISyntaxException {
+    public HttpResponse get(List<String> id, List<NameValuePair> params) throws InterruptedException, IOException, URISyntaxException {
+        RequireKeys.requireKeys(id, "id");
         return ApiClient.getInstance().getRequest("/meetings/"+id, params);
     }
 
-    public HttpResponse update(String id, List<NameValuePair> params, Map<String, Object> data) throws InterruptedException, IOException, URISyntaxException {
+    public HttpResponse update(List<String> id, List<NameValuePair> params, Map<String, Object> data) throws InterruptedException, IOException, URISyntaxException {
+        if(data.get("start_time") != null) {
+            data.put("start_time", DateToString.dateToString(data));
+        }
+        RequireKeys.requireKeys(id, "id");
         return ApiClient.getInstance().patchRequest("/meetings/"+id, params, data);
     }
 
-    public HttpResponse delete(String id, List<NameValuePair> params) throws InterruptedException, IOException, URISyntaxException {
+    public HttpResponse delete(List<String> id, List<NameValuePair> params) throws InterruptedException, IOException, URISyntaxException {
+        RequireKeys.requireKeys(id, "id");
         return ApiClient.getInstance().deleteRequest("/meetings/"+id, params);
     }
 }
