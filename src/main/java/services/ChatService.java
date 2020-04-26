54c234d68e0ade54845934a7c6b4585489b45c97
Nicolas Grantham <nicalgrant@gmail.com>
2020-04-26 15:03:55 -0700

    method to return channel by name

--- src/main/java/services/ChatService.java
+++ src/main/java/services/ChatService.java
@@ -2,6 +2,13 @@ package services;
 
 import components.ChatChannelComponent;
 import components.ChatMessageComponent;
+import models.Channel;
+import models.ChannelPage;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatService {
 
@@ -15,5 +22,18 @@ public class ChatService {
 
     public void sendMessage(String channel, String message) {
 
+    }
+
+    private Channel findByChannelName(String channelName) throws InterruptedException, IOException, URISyntaxException {
+
+        List<String> channelObj = new ArrayList<>();
+        ChannelPage channelPage = chatChannels.listChannels(null);;
+
+        for(Channel channel: channelPage.getChannels()) {
+            if(channel.getName().equals(channelName)) {
+                return channel;
+            }
+        }
+        return null;
     }
 }
