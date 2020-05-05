337ea8f02da2ecb8bf932f6be0e9a19e3a0ec289
Nicolas Grantham <nicalgrant@gmail.com>
2020-05-05 16:43:21 -0700

    update overwrited changes

--- src/main/java/listeners/ChatListener.java
+++ src/main/java/listeners/ChatListener.java
@@ -9,18 +9,20 @@ import models.Message;
 import services.ChatService;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class ChatListener {
+public class ChatListener extends Listener {
 
     private ChatService chatService;
     private static final int CALL_RATE = 10;
 
     public ChatListener() {
+        super();
         this.chatService = new ChatService();
     }
 
@@ -59,8 +61,8 @@ public class ChatListener {
     public void onMessageUpdate(String channelName, MessageCallbackInterface callback) {
         ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
-        CopyOnWriteArrayList<Message> messages = new CopyOnWriteArrayList<>();
-        executorService.scheduleAtFixedRate(() -> {
+        List<Message> messages = new ArrayList<>();
+        registerEvent(() -> {
             LocalDate toDate = LocalDate.now();
             LocalDate fromDate = toDate.minusDays(5);
             try {
@@ -79,7 +81,7 @@ public class ChatListener {
             } catch (InvalidComponentException | InvalidArgumentException exception) {
                 exception.printStackTrace();
             }
-        }, 0, CALL_RATE, TimeUnit.SECONDS);
+        });
     }
 
     public void onNewMember(String channelName, MemberCallbackInterface callback) {
@@ -107,7 +109,7 @@ public class ChatListener {
         }, 0, CALL_RATE, TimeUnit.SECONDS);
     }
 
-    private int findMessageById(CopyOnWriteArrayList<Message> messages, String messageId) {
+    private int findMessageById(List<Message> messages, String messageId) {
         for (int i = 0; i < messages.size(); i++) {
             if (messages.get(i).getId().equals(messageId)) {
                 return i;
@@ -116,5 +118,4 @@ public class ChatListener {
 
         return -1;
     }
-
 }
