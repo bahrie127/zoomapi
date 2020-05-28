6e48b6d65cb1e6da3113dd1402f0e9fe37d31b64
Rafael Oliveira <bellotti.rafael@gmail.com>
2020-05-28 16:29:47 -0700

    added getmethod to cachedchatchannelcomponent

--- src/main/java/bots/OAuthBot.java
+++ src/main/java/bots/OAuthBot.java
@@ -38,6 +38,7 @@ public class OAuthBot {
         client.getChatListener().onMessageUpdate("test", (message) -> System.out.println("Message updated: " + gson.toJson(message)));
         client.getChatListener().onNewMember("test", (member) -> System.out.println("New member: " + gson.toJson(member)));*/
 
-        client.getChatChannels().createChannel("cached channel", 1, new ArrayList<String>(Arrays.asList("nicalgrant@gmail.com")));
+        //client.getChatChannels().createChannel("cached channel", 1, new ArrayList<String>(Arrays.asList("nicalgrant@gmail.com")));
+        System.out.println(gson.toJson(client.getChatChannels().getChannel("97601359-20b6-4b64-a445-177b3231b1c0")));
     }
 }
--- src/main/java/components/CachedChatChannelComponent.java
+++ src/main/java/components/CachedChatChannelComponent.java
@@ -4,9 +4,12 @@ import entities.ChannelEntity;
 import exceptions.InvalidComponentException;
 import exceptions.InvalidEntityException;
 import models.Channel;
+import models.ChannelCollection;
 import repositories.ChannelRepository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class CachedChatChannelComponent {
 
@@ -17,16 +20,56 @@ public class CachedChatChannelComponent {
         this.channelRepository = new ChannelRepository();
     }
 
+    public ChannelCollection listChannels(Map<String, Object> params) throws InvalidComponentException {
+        return chatChannelComponent.listChannels(params);
+    }
+
     public Channel createChannel(String name, int type, List<String> members) throws InvalidComponentException {
         Channel channel = chatChannelComponent.createChannel(name, type, members);
-        ChannelEntity channelEntity = new ChannelEntity();
-
-        channelEntity.setId(channel.getId());
-        channelEntity.setType(type);
-        channelEntity.setName(name);
+        ChannelEntity channelEntity = modelToEntity(channel);
 
         this.channelRepository.save(channelEntity);
 
         return channel;
     }
+
+    public Channel getChannel(String channelId) throws InvalidComponentException {
+        Optional<ChannelEntity> optionalCachedEntity = this.channelRepository.findById(channelId);
+
+        if (optionalCachedEntity.isPresent()) {
+            ChannelEntity cachedEntity = optionalCachedEntity.get();
+            Channel cachedChannel = entityToModel(cachedEntity);
+
+            return cachedChannel;
+        }
+
+        Channel channel = chatChannelComponent.getChannel(channelId);
+        ChannelEntity channelEntity = modelToEntity(channel);
+
+        this.channelRepository.save(channelEntity);
+
+        return channel;
+    }
+
+
+    private Channel entityToModel(ChannelEntity entity) {
+        Channel channel = new Channel();
+
+        channel.setId(entity.getId());
+        channel.setName(entity.getName());
+        channel.setType(entity.getType());
+
+        return channel;
+    }
+
+    private ChannelEntity modelToEntity(Channel model) {
+        ChannelEntity entity = new ChannelEntity();
+
+        entity.setId(model.getId());
+        entity.setName(model.getName());
+        entity.setType(model.getType());
+
+        return entity;
+    }
+
 }
