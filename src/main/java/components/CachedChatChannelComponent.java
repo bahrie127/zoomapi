a3e44c99aa353ad87d9a3c18b62627132ec9a7b6
Nicolas Grantham <nicalgrant@gmail.com>
2020-05-28 16:16:10 -0700

    delete channel

--- src/main/java/components/CachedChatChannelComponent.java
+++ src/main/java/components/CachedChatChannelComponent.java
@@ -29,4 +29,9 @@ public class CachedChatChannelComponent {
 
         return channel;
     }
+
+    public void deleteChannel(String channelId) throws InvalidComponentException {
+        chatChannelComponent.deleteChannel(channelId);
+        this.channelRepository.remove(channelId);
+    }
 }
