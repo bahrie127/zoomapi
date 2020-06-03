9593d0dad0e179e666fc900c622a05e9ac6e73b9
Rafael Oliveira <bellotti.rafael@gmail.com>
2020-06-03 00:18:36 -0700

    optimized dateutil imports

--- src/main/java/util/DateUtil.java
+++ src/main/java/util/DateUtil.java
@@ -2,7 +2,9 @@ package util;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.*;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
